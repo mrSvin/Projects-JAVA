03aa6e5f00ec6205fe19ad9b03f9649afd75418f
mrSvin <vipal921@gmail.com>
2021-06-24 16:35:51 +0300

    commit

--- Computer/src/Computer.java
+++ Computer/src/Computer.java
@@ -1,10 +1,10 @@
 public class Computer {
 
-    private String vendor;
-    private String name;
+    private String vendor="China";
+    private String name ="IBM";
     static int allWeight=0;
 
-    public Computer (cpuType cpu, ddrType ddr, hddType hdd, keyboardType keyboard, monitorType monitor, String vendor, String name) {
+    public Computer (cpuType cpu, ddrType ddr, hddType hdd, keyboardType keyboard, monitorType monitor) {
         new CPU().cpu(cpu);
         new DDR().ddr(ddr);
         new HDD().hdd(hdd);
--- Computer/src/Main.java
+++ Computer/src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Ноутбук 1:");
-        Computer computer = new Computer(cpuType.intel, ddrType.ddr3, hddType.SSD, keyboardType.touch, monitorType.IPS, "Japan", "IBM");
+        Computer computer = new Computer(cpuType.intel, ddrType.ddr3, hddType.SSD, keyboardType.touch, monitorType.IPS);
         System.out.println(computer);
         System.out.println("\n" + "Ноутбук 2:");
-        Computer computer2 = new Computer(cpuType.amd, ddrType.ddr3, hddType.HDD, keyboardType.touch, monitorType.TN, "China", "Lenovo");
+        Computer computer2 = new Computer(cpuType.amd, ddrType.ddr3, hddType.HDD, keyboardType.touch, monitorType.TN);
         System.out.println(computer2);
         System.out.println("\n" + "Ноутбук 3:");
-        Computer computer3 = new Computer(cpuType.amd, ddrType.ddr2, hddType.HDD, keyboardType.mechanical, monitorType.VA, "Russia", "iRu");
+        Computer computer3 = new Computer(cpuType.amd, ddrType.ddr2, hddType.HDD, keyboardType.mechanical, monitorType.VA);
         System.out.println(computer3);
 
     }
