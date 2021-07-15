ce404535c8e7198fcd480ff9122111408e98c5cf
mrSvin <vipal921@gmail.com>
2021-07-15 22:39:24 +0300

    commit

--- 13_FilesAndNetwork/11.4_jsoup/src/main/java/Main.java
+++ 13_FilesAndNetwork/11.4_jsoup/src/main/java/Main.java
@@ -14,7 +14,8 @@ import java.nio.file.Paths;
 
 public class Main {
 //For new commit
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         ImageParser();
     }
 
