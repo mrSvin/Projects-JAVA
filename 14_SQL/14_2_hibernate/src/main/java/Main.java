0c49771005676bce16d5e93aff023157b3ab1925
mrSvin <vipal921@gmail.com>
2021-07-16 17:45:34 +0300

    commit 14.2 update

--- 14_SQL/14_2_hibernate/src/main/java/Main.java
+++ 14_SQL/14_2_hibernate/src/main/java/Main.java
@@ -1,34 +1,8 @@
-import com.mysql.cj.jdbc.Driver;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.mapping.MetadataSource;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
-//for new commit
 public class Main {
     public static void main(String[] args) {
-
-        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
-        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
-        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
-        Session session = sessionFactory.openSession();
-        //Course course = session.get(Course.class, 2);
-        Course course = session.get(Course.class, 4);
-        System.out.println("Название курса: " + course.getName() + " количество студентов: " + course.getStudentsCount());
-
-        sessionFactory.close();
-
+        Hibernate hibernate = new Hibernate();
+        hibernate.HibernateOperations();
     }
-
-
 
 
 }
