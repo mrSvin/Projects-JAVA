72a3ab576af569e5310d7917ea0542c4fd6d4c45
mrSvin <vipal921@gmail.com>
2021-07-23 10:51:17 +0300

    commit 17.1 java

--- 17_Spring/17_1/src/main/java/main/DefaultController.java
+++ 17_Spring/17_1/src/main/java/main/DefaultController.java
@@ -1,32 +1,16 @@
 package main;
 
-import java.util.ArrayList;
 import java.util.Date;
 
-import main.model.Book;
-import main.model.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-//@Controller
+//For new commit
 @RestController
 public class DefaultController {
-
-//    private final BookRepository bookRepository;
-//
-//    @Value("${someParameter.value}")
-//    private Integer someParameter;
-//
-//    public DefaultController(BookRepository bookRepository) {
-//        this.bookRepository = bookRepository;
-//    }
 
     @RequestMapping("/")
     public String index() {
         return (new Date().toString() + ", рандомное число: " + Math.random()*1000);
     }
 }
+//For new commit
--- 17_Spring/17_1/src/main/java/main/Main.java
+++ 17_Spring/17_1/src/main/java/main/Main.java
@@ -2,7 +2,7 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//For new commit
 @SpringBootApplication
 public class Main {
 
@@ -11,3 +11,4 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 }
+//For new commit
