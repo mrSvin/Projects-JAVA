803634add17dad90ef740216e775d3861c1f96e1
Alexey Vasilyev <59051004+mrSvin@users.noreply.github.com>
2021-08-11 04:07:53 +0300

    Delete 08_Strings directory

--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-  public static void main(String[] args) {
-
-    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
-  }
-
-}
